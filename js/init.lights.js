79be4b05c7a40af8a077d459e0c9e1c6c95f1bb7
Gwennael Buchet <gwennael.buchet@gmail.com>
2017-04-05 20:08:57 +0200

    [Improve] Extract Lights initialization functions into a separated file

---
+++ js/init.lights.js
@@ -0,0 +1,14 @@
+/**
+ * Created by gwennael.buchet on 05/04/17.
+ */
+
+function initLights() {
+    /*gl.uniform3f(prg.uLightDirection, 0.0, -1.0, -1.0);
+     gl.uniform4fv(prg.uLightAmbient, [0.03, 0.03, 0.03, 1.0]);
+     gl.uniform4fv(prg.uLightDiffuse, [1.0, 1.0, 1.0, 1.0]);
+     gl.uniform4fv(prg.uLightSpecular, [1.0, 1.0, 1.0, 1.0]);
+     gl.uniform4fv(prg.uMaterialAmbient, [1.0, 1.0, 1.0, 1.0]);
+     gl.uniform4fv(prg.uMaterialDiffuse, [0.5, 0.8, 0.1, 1.0]);
+     gl.uniform4fv(prg.uMaterialSpecular, [1.0, 1.0, 1.0, 1.0]);
+     gl.uniform1f(prg.uShininess, 230.0);*/
+}
--- js/inits.js
+++ js/inits.js
@@ -20,14 +20,3 @@ function initGL() {
         }
     });
 }
-
-function initLights() {
-    /*gl.uniform3f(prg.uLightDirection, 0.0, -1.0, -1.0);
-     gl.uniform4fv(prg.uLightAmbient, [0.03, 0.03, 0.03, 1.0]);
-     gl.uniform4fv(prg.uLightDiffuse, [1.0, 1.0, 1.0, 1.0]);
-     gl.uniform4fv(prg.uLightSpecular, [1.0, 1.0, 1.0, 1.0]);
-     gl.uniform4fv(prg.uMaterialAmbient, [1.0, 1.0, 1.0, 1.0]);
-     gl.uniform4fv(prg.uMaterialDiffuse, [0.5, 0.8, 0.1, 1.0]);
-     gl.uniform4fv(prg.uMaterialSpecular, [1.0, 1.0, 1.0, 1.0]);
-     gl.uniform1f(prg.uShininess, 230.0);*/
-}
