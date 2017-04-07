f0c787fd86bbd536c869e20c542155b32df23acf
Gwennael Buchet <gwennael.buchet@gmail.com>
2017-04-07 17:05:13 +0200

    [Improve] Move shaders folder to the root

--- js/globals.js
+++ js/globals.js
@@ -3,44 +3,47 @@
  */
 
 let gl;
-const canvas = document.getElementById("scene");
+const canvas        = document.getElementById("scene");
 let canvasDimension = {};
 
 let mvMatrix = mat4.create();
-let pMatrix = mat4.create();
+let pMatrix  = mat4.create();
+let nMatrix  = mat4.create();
 
 let sceneTranslation = vec3.fromValues(0.0, 0.0, -10.0);
-let sceneRotation = mat4.create();
+let sceneRotation    = mat4.create();
 
 let programsSources = [
-    {"name": "white", "vs": "js/shaders/flat/vs_simple.glsl", "fs": "js/shaders/flat/fs_simple.glsl"},
-   // {"name": "white", "vs": "js/shaders/phong/vs_phong.glsl", "fs": "js/shaders/phong/fs_phong.glsl"}
+	{"name": "white", "vs": "shaders/flat/vs_simple.glsl", "fs": "shaders/flat/fs_simple.glsl"},
+	//{"name": "phong", "vs": "shaders/phong/vs_phong.glsl", "fs": "shaders/phong/fs_phong.glsl"}
 ];
-let shaderPrograms = {};
+let shaderPrograms  = {};
 
 let meshSources = {
-    'Bootle': 'models/bottle.obj',
-    'Driver': 'models/driver.obj',
-    'Mei': 'models/mei.obj',
-    'Skelout': 'models/internal_skelout_full.obj',
-    'SpeedCar': 'models/SpeedCar.obj',
-    'Teapot': 'models/teapot.obj',
-    'Teddy': 'models/teddy.obj',
-    'Tuna': 'models/tuna.obj',
-    'Vi': 'models/vi.obj',
-    'X-Wing': 'models/x-wing.obj'
+	'Bootle'  : 'models/bottle.obj',
+	'Driver'  : 'models/driver.obj',
+	'Mei'     : 'models/mei.obj',
+	'Skelout' : 'models/internal_skelout_full.obj',
+	'SpeedCar': 'models/SpeedCar.obj',
+	'Teapot'  : 'models/teapot.obj',
+	'Teddy'   : 'models/teddy.obj',
+	'Tuna'    : 'models/tuna.obj',
+	'Vi'      : 'models/vi.obj',
+	'X-Wing'  : 'models/x-wing.obj'
 };
-let meshes = {};
+let meshes      = {};
 
-let texturesURLs = [{"zenika_beach": "img/zenika_beach.jpg"}];
-let textures = {};
+let texturesURLs = [
+	{"zenika_beach": "img/zenika_beach.jpg"}
+];
+let textures     = {};
 
 let params = {
-    currentShaderProgram: null,
-    currentMesh: null,
+	currentShaderProgram: null,
+	currentMesh         : null,
 
-    isAnimated: true,
-    renderingMode: null
+	isAnimated   : true,
+	renderingMode: null
 };
 
 let animatedLoader = new AnimatedLoader("animatedLoader");
