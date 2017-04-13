ca6f37b215986c8a91450f5da14fb32911a1b209
Gwennael Buchet <gwennael.buchet@gmail.com>
2017-04-13 21:03:33 +0200

    [Improve] true InitLight

--- js/init.lights.js
+++ js/init.lights.js
@@ -2,13 +2,10 @@
  * Created by gwennael.buchet on 05/04/17.
  */
 
-function initLights(program) {
-	gl.uniform3f(program.uLightDirection, 0.0, 10.0, 10.0);
-	gl.uniform4fv(program.uLightAmbient, [0.03, 1, 0.03, 1.0]);
-	gl.uniform4fv(program.uLightDiffuse, [1.0, 1.0, 1.0, 1.0]);
-	gl.uniform4fv(program.uLightSpecular, [1.0, 1.0, 1.0, 1.0]);
-	gl.uniform4fv(program.uMaterialAmbient, [1.0, 1.0, 1.0, 1.0]);
-	gl.uniform4fv(program.uMaterialDiffuse, [0.5, 0, 0, 1.0]);
-	gl.uniform4fv(program.uMaterialSpecular, [1.0, 1.0, 1.0, 1.0]);
-	gl.uniform1f(program.uShininess, 230.0);
+function initLights() {
+	for (let i = 0; i < NB_LIGHT; i++) {
+		let light = new Light();
+
+		lights.push(light);
+	}
 }
